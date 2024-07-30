5df84302c4bb5834d01ff6bb6d5a02940c412029
Suparn001 <suparngautam@gmail.com>
2024-07-30 11:24:20 +0530

    commited

---
+++ variables.php
@@ -0,0 +1,21 @@
+<?php
+// for declaring a varibale $ symbol is used first with the desired name you want;
+// you hva to use variable name alogn with dollar where ever you want to use that variable
+
+$a  = "hello<br>";
+
+echo $a;
+echo $a;
+echo $a;
+echo $a;
+echo $a;
+
+
+
+
+
+
+
+
+
+?>
