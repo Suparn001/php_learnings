4d54c898714c3af74a0b3b0bef4b10fcaeeedc56
Suparn001 <suparngautam@gmail.com>
2024-07-30 11:06:55 +0530

    done

---
+++ echo.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "helo<br>";
+echo "how are you<br>";
+echo "hello"," how are you<br>";
+echo "hello"." how are you<br>";
+ // here  , amd .(concatenation) both have different meaning 
+ // . makes two strings into 1 single string where as , does not do that, it make it the same they are
+
+ echo "<h1><i>hello</i></h1>"; // you can use the tags of html with in "" these commas
+ echo 2345.789;
+
+ // using print 
+
+ print "<br>hello how are you<br>";
+ print 2345.890;
+?>
