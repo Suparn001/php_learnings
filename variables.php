746246af18db334ca7d7ad89ef1d3190311b8882
Suparn001 <suparngautam@gmail.com>
2024-07-30 17:40:51 +0530

    done

--- variables.php
+++ variables.php
@@ -10,6 +10,23 @@ echo $a;
 echo $a;
 echo $a;
 
+/* there is another type of varibale that is CONSTANT VARIABLE
+these CONSTANT VARIBALE have global scope. these are case sensitive by default.
+to declare constant variable "define"  is used. Example is ->
+define(_,_,_)
+this takes three parameters-> first is name of variable, 2nd is value of variable, 3rd is fir case sensitve. by default it is case sensitive.
+3rd argument takes only boolean value true and false only for case sensitve.
+*/
+define("greetings","heelo"); // here by default value is true for case senstive
+echo greetings."<br>";
+
+
+
+define("GREET", "Welcome!");
+echo GREET;
+
+
+
 
 
 
