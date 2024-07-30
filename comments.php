3e40aba2ee59dc0c9f0c9f899ca47add0a551485
Suparn001 <suparngautam@gmail.com>
2024-07-30 12:05:45 +0530

    done

---
+++ comments.php
@@ -0,0 +1,13 @@
+<?php
+// comments are to give information about functioning of code  or it can be used to describe the problem
+// basicaally comments arew non executable code.
+// to provide cvomments there are diiferent tyoes of comments such as single line comment and multi line comment
+// for comment (#, //, /**/) are used
+// for single line comment // or # are used
+// for multiline comment /**/ used. Example given below->
+
+/*hi this tpic uis about comments
+this is about the usage of comments in your code*/
+
+
+?>
